8f239ec668f83f58e7ede21f49d06ac7a1d74d68
Nishit Mehta <nvmaheta.mca@gmail.com>
2013-05-15 04:27:40 -0600

    Update preloader.js

--- js/preloader.js
+++ js/preloader.js
@@ -1,37 +1,89 @@
-<!doctype html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <title>Image Preloader</title>
-  <meta name="description" content="Image Preloade">
-  <meta name="author" content="Nishit Mehat">
-  <script src="js/jquery-1.9.0.min.js" ></script>
-  <script src="js/preloader.js" ></script>
-  <script>
-    $( document ).ready(function(){
-		try{
-			$('.preload').prepreLoader();
-		}catch( e ){
-			alert( e.message );
-		}
-	});
-  </script>
-</head>
-<body>
-<div id="main" >
 
-	<div style="width:100%;">
-    	<div style="width:300px; height:200px; float:left; position:relative;"> 
-        	<img src="images/1264107698bi3up6.jpg"  class="preload" style="width:100%" />
-        </div>
-		<div style="width:300px; height:200px; float:left; position:relative;">
-        	 <img src="images/12923298509657AP.jpg" style="width:100%" class="preload" />
-        </div>
-        <div style="width:300px; height:200px; float:left; position:relative;">
-        	<img src="http://www.foamwork.net/images/fw4-6.jpg" style="width:100%" class="preload" />
-        </div>    	
-    </div>
-    
-</div>
-</body>
-</html>
+/********************************************************************************************************
+ *																										*
+ 										Created BY : Nishit Mehta
+										Date : 15 - May - 2013
+										contact : nvmaheta.mca@gmail.com
+ *																										*
+ *******************************************************************************************************/	
+
+/*
+	This is my first jquery plugin :)
+	
+	load images with nice fadein effect.
+	it calcualte actual time to load image once iamge is load loader is removed and iamge display with fadeIn effect.
+	
+	-> How to use plugin
+	   assign same classname to all images and use below code. 	
+	   $(".className").preLoader();
+*/
+
+(function ( $ ) {
+	
+	var preLoaderOptions = {
+		  "onComplete" : function(){},
+		  "image"      : "images/preloader.gif"
+	 }	
+
+	var preLoader = {
+		counterOverlayOBJ : '',
+		counterOverlayOBJ : '',
+		titleOverlayOBJ   : '',
+		backOverlayOBJ    : '',
+		titleHolderOverlayOBJ : '',
+		preLoaderCounter : -1,
+		preLoaderPer  : '',
+		preLoaderText :  '',	
+		'init' : function( elementOBJ ){
+			  preLoader.createPreloader(elementOBJ);
+			  var loadImage = new Image();
+			  loadImage.src = $(elementOBJ).attr('src');
+			  loadImage.onload = function(){
+			 	preLoader.overlayDestory(elementOBJ)
+			  }
+		},
+		'createPreloader' : function(elementOBJ){
+			
+			preLoader.createDiv = $('<div class="preloaderImg"></div>').css({
+				margin:'auto',
+				position:'absolute',
+				top:'50%',
+				left:'50%',
+				zindex:'11111'
+			}).appendTo($(elementOBJ).parent());
+			
+			preLoader.imageTag = $('<img />').attr('src',preLoaderOptions.image).appendTo(preLoader.createDiv);
+			
+		},
+		'overlayDestory' : function(elementOBJ){
+			
+			$(elementOBJ).parent().find('.preloaderImg').fadeOut('slow',function(){
+					$(this).remove();
+					$(elementOBJ).fadeIn('slow');
+					preLoaderOptions.onComplete();
+			});			
+		}
+	} 
+
+		$.fn.prepreLoader = function( options ) {
+		if(options) {
+			 $.extend(preLoaderOptions, options );
+		}
+		
+		this.each(function() {
+			$(this).hide();
+			preLoader.init(this);
+		});
+	};
+}( jQuery ));
+
+
+
+
+
+
+
+
+
+
+
